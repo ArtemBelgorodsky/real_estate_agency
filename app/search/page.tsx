829cf00a32bb1dd4e48d7081b603c604a57ae410
artem <artem-belgorodkiy@yandex.ru>
2024-09-16 19:31:08 +0300

    v2

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,20 +1,23 @@
 'use client';
 import { TableOffers } from '@/components';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default async function Search() {
+export default async function SearchingOffers() {
   const searchParams = useSearchParams();
-  console.log(searchParams);
+
   const searchOffers = await fetch(
     `https://b580138023681732.mokky.dev/items?${searchParams}`
   );
   const items = await searchOffers.json();
 
   return (
-    <div className="bg-gray-100 font-sans">
-      <main className="p-8">
-        <TableOffers items={items} />
-      </main>
-    </div>
+    <Suspense>
+      <div className="bg-gray-100 font-sans">
+        <main className="p-8">
+          <TableOffers items={items} />
+        </main>
+      </div>
+    </Suspense>
   );
 }
